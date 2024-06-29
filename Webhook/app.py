13537c08ecbe1c3b94daa1da9786f3171a840c06
Vikranth Sunkarpally <50102593+vikranth18devops@users.noreply.github.com>
2024-06-29 18:52:59 +0530

    Update app.py

--- Webhook/app.py
+++ Webhook/app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/trigger-call', methods=['GET', 'POST'])
 def trigger_call():
-    account_sid = '<acc-sid>'
-    auth_token = '<acc-auth>'
+    account_sid = 'AC2abfc9c9a1c60b9ac8d86f55594802fa'
+    auth_token = '7088971bc5cc90115d86533bdb390a04'
     client = Client(account_sid, auth_token)
 
     # List of numbers to call
-    numbers_to_call = ['<number>', '<number>']
+    numbers_to_call = ['+919052113411', '<number>']
     call_sids = []  # To store Call SIDs
 
     for number in numbers_to_call:
         call = client.calls.create(
                             twiml='<Response><Say>"change Alert message here"</Say></Response>',
                             to=number,
-                            from_='<twilio-number>'
+                            from_='+13203728367'
                         )
         call_sids.append(call.sid)
 
